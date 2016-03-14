b7ac93175ab085d2f626cf30459753feaf00ee7e
Diony C <dionyc@Dionys-iMac.home>
2016-03-14 15:26:34 -0400

    First Commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry
+} = React;
+
+var WeatherProject = require('./WeatherProject');
+AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
